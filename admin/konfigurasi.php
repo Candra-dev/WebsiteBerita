53813f6509545333dadba49e7092e92503e6f70a
Candra-dev <84716211+Candra-dev@users.noreply.github.com>
2022-02-06 00:06:40 +0700

    register dan password

--- admin/konfigurasi.php
+++ admin/konfigurasi.php
@@ -16,7 +16,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Dashboard - SB Admin</title>
+    <title>Dashboard - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="../css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
